e66711e807931bd4886246bbdabcc883a06c1e35
Aditya <aadipatle6@gmail.com>
2025-06-16 12:32:48 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'redux_dep.git',
+  base:'https://github.com/Aadipatle/redux_dep.git',
   plugins: [react()],
 })
